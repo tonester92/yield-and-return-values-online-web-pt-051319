7443ce84eeb16723d5856a1b097c7e65d650ed51
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-23 21:32:43 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,12 +2,8 @@ require 'pry'
 
 def hello(array)
   i = 0
-  collection = []
   while i < array.length
-    collection << yield(array[i])
+    yield(array[i])
     i += 1
   end
-  collection
 end
-
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
